7d425297f6559ac9364d78c3d187c1be9dca4c61
Priscila Heller <priscila.x.p.r@gmail.com>
2019-04-03 11:31:29 -0400

    everything works but accuweather stuff. No css done either

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,112 +1,160 @@
+$(document).ready(function () {
 var topics = ["Chicago","New York","Los Angeles","San Francisco","Louisiana","Austin","Nashville","Atlanta","Portland","Sioux Falls"];
 
-$(document).ready(function() {
+function createBtns() {
+  $("#btns").empty();
 
-  function createButtons() {
-    $("#buttons-view").empty();
+  for (var i = 0; i < topics.length; i++) {
+      var gifButton = $("<button>");
 
-    for (var i = 0; i < topics.length; i++) {
-      var btn = $("<button>");
-        btn.addClass("gif");
-          btn.attr("data-name", topics[i]);
-            btn.text(topics[i]);
-              $("#buttons-view").append(btn);
-    }
+      gifButton.attr("ID", "gifArrayBtns");
+      gifButton.attr("class", "btn btn-primary btn-space")
+      gifButton.attr("data-button", topics[i]);
+      gifButton.text(topics[i]);
+
+      $("#btns").append(gifButton);
   }
+}
 
-  createButtons();
+createBtns();
 
-  $("#add-city").on("click", function(event) {
-    event.preventDefault();
-      var cityGif = $("#city-input").val().trim();
-        topics.push(cityGif);
-        $("#city-input").val('');
-          createButtons();
-  });
+function renderFavs(favs) {
+  $("#gif-favorites").empty();
 
-  // function getForecast () {
-  //   var queryURL3 = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/" + results2 + "?apikey=uwu2fjRO7VYtbr9dCRkykSPy7wJOn3cF"
-  //   console.log(queryURL3);
-  // }
+  for (var i = 0; i < favs.length; i++) {
+      var favsImg = $("<img>")
+      favsImg.attr("src", favs[i]);
+      favsImg.attr("style", "width: 150px; height: 150px")
+      favsImg.addClass("btn-space")
 
+      $("#gif-favorites").append(favsImg)
+  }
+}
 
+$("#add-keyword").click(function () {
+  var keywordPush = $("#keyword-term").val();
+  topics.push(keywordPush);
+  createBtns();
+  $("#keyword-term").val("");
+})
 
-  $("#buttons-view").on("click", ".gif", function(event) {
-    event.preventDefault();
-    var selectedCity = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + selectedCity + "&api_key=UxNR1uNih1F65bA3EEK3M4XZnDrOhr2A&limit=10";
-    console.log("Query " + queryURL);
-    var queryURL2 = "http://dataservice.accuweather.com/locations/v1/cities/search?q=" + selectedCity + "&apikey=uwu2fjRO7VYtbr9dCRkykSPy7wJOn3cF";
-    var queryURL3 = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/" + results2 + "?apikey=uwu2fjRO7VYtbr9dCRkykSPy7wJOn3cF"
-    var results;
-    var results2;
-    var results3;
- 
+$(document).on("click", "#gifArrayBtns", function () {
+  // var results2;
+  // var results3;
+  var searchQuery = $(this).attr("data-button");
+  var giphyAPIKey = "api_key=UxNR1uNih1F65bA3EEK3M4XZnDrOhr2A";
+  // var accuweatherAPIKey = "apikey=uwu2fjRO7VYtbr9dCRkykSPy7wJOn3cF";
+  var giphyQueryURL = "https://api.giphy.com/v1/gifs/search?" + giphyAPIKey + "&q=" + searchQuery + "&limit=10";
+  // var locationQueryURL = "http://dataservice.accuweather.com/locations/v1/cities/search?q=" + searchQuery + "&" + accuweatherAPIKey;
+  // var weatherQueryURL = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/" + results2 + "?" + accuweatherAPIKey;
 
-    console.log("Query " + queryURL2);
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          .then(function(response) {
-            results = response.data;
+  $.ajax({
+      url: giphyQueryURL,
+      method: "GET"
+  }).then(function (response) {
+
+    var gifArray = response.data;
+
+    for (var i = 0; i < gifArray.length; i++) {
+      var gifDiv = $("<div>");
+      gifDiv.addClass("card btn-space mx-auto")
+      gifDiv.attr("style", "width: 20em")
+      var gifDivBody = $("<div>")
+      gifDivBody.addClass("card-body")
+
+      var gifRating = gifArray[i].rating;
+      var gifTitle = gifArray[i].title;
+      var gifTitleShort = gifTitle.slice(0, 15);
+
+      var title = $("<strong>").text(gifTitleShort.toUpperCase() + "...");
+      title.attr("class", "card-title")
+      var rating = $("<p>").text("Rating: " + gifRating.toUpperCase());
+      rating.attr("class", "card-body")
+
+      var originalDownload = gifArray[i].images.original.url;
+      var downloadBtn = $("<a>").text("Download");
+      downloadBtn.attr("href", originalDownload);
+      downloadBtn.attr("download", "giphy.gif");
+      downloadBtn.addClass("btn btn-primary btn-space");
+
+      var gifAnimate = gifArray[i].images.fixed_height.url
+      var gifStill = gifArray[i].images.fixed_height_still.url
+      var gifImage = $("<img>");
+      gifImage.attr("src", gifStill);
+      gifImage.attr("data-still", gifStill)
+      gifImage.attr("data-animate", gifAnimate)
+      gifImage.attr("data-state", "still")
+      gifImage.addClass("card-img-top gif")
+
+      var heartSpan = $("<i>");
+      heartSpan.addClass("fas fa-heart");
+      heartSpan.attr("aria-hidden", "true")
+      heartSpan.attr("span-image", gifAnimate)
+
+        // $.ajax({
+        // url: locationQueryURL,
+        // method: "GET"
+        // })
+        // .then(function(response2) {
+        //   results2 = response2[0].Key;
+        //   console.log("City key" + results2);
           
-            console.log(results);
-  
-            for (var i = 0; i < results.length; i++) {
-              var rating = results[i].rating;
-              var title = results[i].title;
-              var weather = $("<p>");
-              var totalSeasons;
-  
-              var ratingParagraph = $("<p>").text("Rating: " + rating );
-              var titleParagraph = $("<p>").text("Title: " + title );
-  
-              var cityImage = $("<img>");
-              cityImage.attr("src", results[i].images.fixed_height_still.url);
-              cityImage.attr("alt", title);
+        //   $.ajax({
+        //     url: weatherQueryURL,
+        //     method: "GET"
+        //   })
+        //     .then(function(response3) {
+        //       console.log(response3);
 
-        $.ajax({
-          url: queryURL2,
-          method: "GET"
-        })
-          .then(function(response2) {
-            results2 = response2[0].Key;
-            console.log("City key" + results2);
             
-          $.ajax({
-            url: queryURL3,
-            method: "GET"
-          })
-            .then(function(response3) {
-              console.log(response3);
-  
-            
-              results3 = response3;
-                   
+        //       results3 = response3;
 
+            gifDiv.append(gifImage);
+            gifDiv.append(gifDivBody);
+            gifDivBody.append(title);
+            gifDivBody.append(rating);
+            gifDivBody.append(downloadBtn);
+            gifDivBody.append(heartSpan)
 
-            });
-
-          
-          });
-      } 
-    }); 
-    });
-  });  
-  
-//this function makes the gif move when there is a mouse hover.
-//$("document").on("click", "img", function() {
-    $("#gifs img").hover(function() {
-      var src = $(this).attr("src");
-    if($(this).hasClass('playing')){
-       //stop
-       $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
-       $(this).removeClass('playing');
-    } else {
-      //play
-      $(this).addClass('playing');
-      $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+            $("#gif-body").prepend(gifDiv);
+        // });
+      // });
     }
   });
+});
 
+$(document).on("click", ".fas", function () {
+  var favImg = $(this).attr("span-image");
+  favs.push(favImg);
+  renderFavs(favs);
+  localStorage.setItem("favs-array", JSON.stringify(favs));
+})
+
+$("#clearFavs").click(function () {
+  localStorage.removeItem("favs-array")
+  renderFavs(favs)
+  favs = []
+  $("#gif-favorites").empty();
+})
+
+$(document).on("click", "img.gif", function () {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+  } else if (state === "animate") {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still")
+  }
+})
+
+var favs = JSON.parse(localStorage.getItem("favs-array"));
+if (!Array.isArray(favs)) {
+  favs = [];
+}
+
+renderFavs(favs);
+
+
+});
