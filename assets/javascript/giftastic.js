b58f743446c3bca76bb70921e3e61fa459f789e3
Priscila Heller <priscila.x.p.r@gmail.com>
2019-04-04 10:47:24 -0400

    changed protocol to https so pages would work correctly

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -44,7 +44,7 @@ $(document).on("click", "#gifArrayBtns", function () {
   var giphyAPIKey = "api_key=UxNR1uNih1F65bA3EEK3M4XZnDrOhr2A";
   var accuweatherAPIKey = "apikey=uwu2fjRO7VYtbr9dCRkykSPy7wJOn3cF";
   var giphyQueryURL = "https://api.giphy.com/v1/gifs/search?" + giphyAPIKey + "&q=" + searchQuery + "&limit=10";
-  var locationQueryURL = "http://dataservice.accuweather.com/locations/v1/cities/search?q=" + searchQuery + "&" + accuweatherAPIKey;
+  var locationQueryURL = "https://dataservice.accuweather.com/locations/v1/cities/search?q=" + searchQuery + "&" + accuweatherAPIKey;
   
 
   $.ajax({
